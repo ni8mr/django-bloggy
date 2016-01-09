14f4f6ab961154a10bfb50dfb17a2a5367472a62
ni8mr <nilpaddo@gmail.com>
2016-01-09 17:53:37 +0600

    Made further changes and saved the database for future references

--- bloggy_project/blog/admin.py
+++ bloggy_project/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+admin.site.register(Post)
+from django.contrib import admin
+from blog.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'created_at')
+
+#admin.site.register(Post, PostAdmin)
--- bloggy_project/blog/models.py
+++ bloggy_project/blog/models.py
@@ -10,4 +10,7 @@ class Post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField()
 
+	def __unicode__(self):
+		return self.title
+
 

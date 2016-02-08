bf4373d0cbf4e331f5012127bf8b181b8d9fe0c4
ni8mr <nilpaddo@gmail.com>
2016-02-08 18:15:31 +0600

    Added a rough view without any url mapping

--- bloggy_project/blog/admin.py
+++ bloggy_project/blog/admin.py
@@ -2,12 +2,9 @@ from django.contrib import admin
 from blog.models import Post
 
 # Register your models here.
-admin.site.register(Post)
-from django.contrib import admin
-from blog.models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
 	list_display = ('title', 'created_at')
 
-#admin.site.register(Post, PostAdmin)
+admin.site.register(Post, PostAdmin)
--- bloggy_project/blog/tests.py
+++ bloggy_project/blog/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from models import Post
+from blog.models import Post
 
 # Create your tests here.
 class PostTests(TestCase):
